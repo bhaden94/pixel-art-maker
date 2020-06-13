7ce2dae52653f2e335369577a93ee78313398fe7
Brady Haden <bhaden94@yahoo.com>
2020-06-12 19:18:55 -0700

    add drag and color ability

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ for (let i = 0; i < 4000; i++) {
   document.getElementById("pixel-container").appendChild(boxDiv);
 }
 
+let currentColor = "rgb(255, 255, 255)"; // starting value is white
+
 // gets current color picked and sets it
 // also sets current color box
-let currentColor = "rgb(255, 255, 255)"; // starting value is white
-document.querySelector("#color-picker").addEventListener("mousedown", (e) => {
+document.querySelector("#color-picker").addEventListener("click", (e) => {
   let currentElement = document.querySelector(`.${e.target.className}`);
   currentColor = getComputedStyle(currentElement).backgroundColor;
   let currentColorBox = document.querySelector(".current-color-box");
@@ -16,30 +17,30 @@ document.querySelector("#color-picker").addEventListener("mousedown", (e) => {
 });
 
 // changes background color of pixel clicked
-let timeout;
-document
-  .querySelector("#pixel-container")
-  .addEventListener("mousedown", (e) => {
-    // if statement to stop border color from being changed
-    console.log(e)
-    if (e.target.id !== "pixel-container") {
-      timeout = setInterval(() => {
-        let clickedPixel = e.target;
-        clickedPixel.style.backgroundColor = currentColor;
-        console.log(e)
-      }, 100);
-    }
-    return false;
+// can hold down and drag to color many at once
+let pixelContainer = document.querySelector("#pixel-container");
+let isDown = false; // for checking during drag events if mouse is still being held down
+pixelContainer.addEventListener("mousedown", (clicked) => {
+  isDown = true;
+  console.log(isDown);
+  if (!isBorder(clicked)) {
+    let clickedPixel = clicked.target;
+    clickedPixel.style.backgroundColor = currentColor;
+    pixelContainer.addEventListener("mouseover", (over) => {
+      if (!isBorder(over) && isDown) {
+        let overPixel = over.target;
+        overPixel.style.backgroundColor = currentColor;
+      }
+    });
+  }
 });
 
-document
-  .querySelector("#pixel-container").addEventListener('mouseup', () => {
-  clearInterval(timeout)
-  return false;
-})
+// stops div borders from beign changed colors
+function isBorder(div) {
+  return div.target.id === "pixel-container";
+}
 
-document
-  .querySelector("#pixel-container").addEventListener('mouseout', () => {
-  clearInterval(timeout)
-  return false;
-})
+// stops coloring on mouseup
+pixelContainer.addEventListener("mouseup", () => {
+  isDown = false;
+});
