b85ae56266d0c9c2c6ce65b77345f199d96293f2
Brady Haden <bhaden94@yahoo.com>
2020-06-12 18:49:25 -0700

    logging on hold mousedown

--- script.js
+++ script.js
@@ -11,17 +11,35 @@ let currentColor = "rgb(255, 255, 255)"; // starting value is white
 document.querySelector("#color-picker").addEventListener("mousedown", (e) => {
   let currentElement = document.querySelector(`.${e.target.className}`);
   currentColor = getComputedStyle(currentElement).backgroundColor;
-  let currentColorBox = document.querySelector('.current-color-box');
-  currentColorBox.style.backgroundColor = currentColor
+  let currentColorBox = document.querySelector(".current-color-box");
+  currentColorBox.style.backgroundColor = currentColor;
 });
 
 // changes background color of pixel clicked
+let timeout;
 document
   .querySelector("#pixel-container")
   .addEventListener("mousedown", (e) => {
     // if statement to stop border color from being changed
+    console.log(e)
     if (e.target.id !== "pixel-container") {
-      let clickedPixel = e.target;
-      clickedPixel.style.backgroundColor = currentColor;
+      timeout = setInterval(() => {
+        let clickedPixel = e.target;
+        clickedPixel.style.backgroundColor = currentColor;
+        console.log(e)
+      }, 100);
     }
-  });
+    return false;
+});
+
+document
+  .querySelector("#pixel-container").addEventListener('mouseup', () => {
+  clearInterval(timeout)
+  return false;
+})
+
+document
+  .querySelector("#pixel-container").addEventListener('mouseout', () => {
+  clearInterval(timeout)
+  return false;
+})
